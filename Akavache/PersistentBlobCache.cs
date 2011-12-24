528ad533a585c0a61dbbe74325d16aa69088f3ab
Paul Betts <paul@paulbetts.org>
2011-12-24 00:13:39 -0500

    Add support for SL IsolatedStorage

--- Akavache/PersistentBlobCache.cs
+++ Akavache/PersistentBlobCache.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Globalization;
 using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Concurrency;
@@ -179,6 +180,7 @@ namespace Akavache
                         filesystem.Delete(GetPathForKey(key));
                     }
                     catch (FileNotFoundException ex) { log.Warn(ex); }
+                    catch (IsolatedStorageException ex) { log.Warn(ex); }
                 });
 
                 actionTaken.OnNext(Unit.Default);
@@ -249,6 +251,7 @@ namespace Akavache
                 .SelectMany(x => x.CopyToAsync(ms, scheduler))
                 .SelectMany(x => AfterReadFromDiskFilter(ms.ToArray(), scheduler))
                 .Catch<byte[], FileNotFoundException>(ex => Observable.Throw<byte[]>(new KeyNotFoundException()))
+                .Catch<byte[], IsolatedStorageException>(ex => Observable.Throw<byte[]>(new KeyNotFoundException()))
                 .Multicast(ret).Connect();
 
             return ret;
