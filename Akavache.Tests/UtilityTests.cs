caa21bc777d66eb42e81291c01cecf76087a3012
Adam Roben <adam@roben.org>
2012-05-24 14:35:38 -0700

    Add a simple test for Utility.CreateRecursive

---
+++ Akavache.Tests/UtilityTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Akavache.Tests
+{
+    public class UtilityTests
+    {
+        public class TheCreateRecursiveMethod
+        {
+            [Fact]
+            public void CreatesDirectories()
+            {
+                string path;
+                using (Utility.WithEmptyDirectory(out path))
+                {
+                    var dir = new DirectoryInfo(Path.Combine(path, @"foo\bar\baz"));
+                    dir.CreateRecursive();
+                    Assert.True(dir.Exists);
+                }
+            }
+        }
+    }
+}
