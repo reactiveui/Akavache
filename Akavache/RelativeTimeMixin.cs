ebad131f23717c708f1c79abea546c7846f9311b
Brendan Forster <me@brendanforster.com>
2012-11-07 21:14:29 +1100

    i'm a grumpy old man and have no time for digging through multiple classes in one code file

---
+++ Akavache/RelativeTimeMixin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+
+#if SILVERLIGHT
+using System.Net.Browser;
+#endif
+
+namespace Akavache
+{
+    public static class RelativeTimeMixin
+    {
+        public static void Insert(this IBlobCache This, string key, byte[] data, TimeSpan expiration)
+        {
+            This.Insert(key, data, This.Scheduler.Now + expiration);
+        }
+
+        public static void InsertObject<T>(this IBlobCache This, string key, T value, TimeSpan expiration)
+        {
+            This.InsertObject(key, value, This.Scheduler.Now + expiration);
+        }
+
+        public static IObservable<byte[]> DownloadUrl(this IBlobCache This, string url, TimeSpan expiration, Dictionary<string, string> headers = null, bool fetchAlways = false)
+        {
+            return This.DownloadUrl(url, headers, fetchAlways, This.Scheduler.Now + expiration);
+        }
+
+        public static void SaveLogin(this ISecureBlobCache This, string user, string password, string host, TimeSpan expiration)
+        {
+            This.SaveLogin(user, password, host, This.Scheduler.Now + expiration);
+        }
+    }
+}
