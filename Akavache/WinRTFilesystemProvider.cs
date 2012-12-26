82585730f7287552cac6338a96d7add841c91110
Paul Betts <paul@paulbetts.org>
2012-12-25 18:52:36 -0800

    Pretty sure await is just broken here, just write this the Functional way

--- Akavache/WinRTFilesystemProvider.cs
+++ Akavache/WinRTFilesystemProvider.cs
@@ -10,11 +10,12 @@ using System.Reactive.Windows.Foundation;
 using System.Text;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using ReactiveUI;
 using Windows.Storage;
 
 namespace Akavache
 {
-    public class SimpleFilesystemProvider : IFilesystemProvider
+    public class SimpleFilesystemProvider : IFilesystemProvider, IEnableLogger
     {
         public IObservable<Stream> SafeOpenFileAsync(string path, FileMode mode, FileAccess access, FileShare share, IScheduler scheduler)
         {
@@ -30,50 +31,28 @@ namespace Akavache
 
         public IObservable<Unit> CreateRecursive(string path)
         {
-            var ret = createRecursiveHelper(path).ToObservable();
-            return ret;
-        }
-
-        async Task createRecursiveHelper(string path)
-        {
             var paths = path.Split('\\');
-            var root = default(string);
 
-            // Find the latest folder that exists
-            for (int i = paths.Count(); i > 0; i--)
-            {
-                var dir = String.Join("\\", paths.Take(i));
+            var firstFolderThatExists = Observable.Range(0, paths.Length - 1)
+                .Select(x =>
+                    StorageFolder.GetFolderFromPathAsync(String.Join("\\", paths.Take(paths.Length - x)))
+                    .ToObservable()
+                    .LoggedCatch(this, Observable.Empty<StorageFolder>()))
+                .Concat()
+                .Take(1);
 
-                try
+            return firstFolderThatExists
+                .Select(x =>
                 {
-                    var sf = await StorageFolder.GetFolderFromPathAsync(dir);
-                    root = sf.Path;
-                    break;
-                }
-                catch (FileNotFoundException ex)
+                    if (x.Path == path) return null;
+                    return new { Root = x, Paths = path.Replace(x.Path + "\\", "").Split('\\')};
+                })
+                .SelectMany(x =>
                 {
-                    continue;
-                }
-            }
-
-            if (root == path) 
-            {
-                return;
-            }
-
-            if (root == null)
-            {
-                throw new FileNotFoundException("Couldn't find the root directory");
-            }
-
-            var folder = await StorageFolder.GetFolderFromPathAsync(root);
-            paths = path.Replace(root + "\\", "").Split('\\');
-
-            // Create the ones that don't exist
-            foreach (var pathSegment in paths)
-            {
-                folder = await folder.CreateFolderAsync(pathSegment);
-            }
+                    if (x == null) return Observable.Return(default(StorageFolder));
+                    return x.Paths.ToObservable().Aggregate(x.Root, (acc, y) => acc.CreateFolderAsync(y).ToObservable().First());
+                })
+                .Select(_ => Unit.Default);
         }
 
         public IObservable<Unit> Delete(string path)
