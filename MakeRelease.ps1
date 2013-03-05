5ac05e7b6c19183f23562d70448131b26e8d2d02
Paul Betts <paul@paulbetts.org>
2013-03-05 15:57:37 -0800

    Add a release script

---
+++ MakeRelease.ps1
@@ -0,0 +1,26 @@
+﻿$Archs = {"Net40", "Net45", "SL5", "WP7", "WP8", "WinRT45", "Mono"}
+$Projects = {"Akavache", "Akavache.Sqlite3", "Akavache.Mobile"}
+
+$SlnFileExists = Test-Path ".\Akavache.sln"
+if ($SlnFileExists -eq $False) {
+    echo "*** ERROR: Run this in the project root ***"
+    exit -1
+}
+
+###
+### Build the Release directory
+###
+
+rmdir -r --force .\Release
+
+foreach-object $Archs | %{mkdir -p ".\Release\$_"}
+
+foreach-object $Archs | %{
+    $currentArch = $_
+    
+    foreach-object $Projects | %{cp -r -fo ".\$_\bin\Release\$currentArch\*" ".\Release\$currentArch"}
+    
+    #ls -r | ?{$_.FullName.Contains("bin\Release\$currentArch") -and $_.Length} | %{echo cp $_.FullName ".\Release\$currentArch"}
+}
+
+ls -r .\Release | ?{$_.FullName.Contains("Clousot")} | %{rm $_.FullName}
