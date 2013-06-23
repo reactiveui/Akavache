7394b3593f50d852439f0f61467c0ed07c818639
Paul Betts <paul@paulbetts.org>
2013-06-22 20:13:04 -0700

    Bring back setting the app name based on the package info

--- Akavache.Mobile/Registrations.cs
+++ Akavache.Mobile/Registrations.cs
@@ -6,7 +6,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#if UIKIT
+using MonoTouch.Foundation;
+using ReactiveUI.Mobile;
+#endif
+
+#if APPKIT
+using MonoMac.Foundation;
+#endif
+
+#if ANDROID
+using Android.App;
+#endif
+
+#if APPKIT
+namespace Akavache.Mac
+#else
 namespace Akavache.Mobile
+#endif
 {
     public class Registrations : IWantsToRegisterStuff
     {
@@ -23,9 +40,14 @@ namespace Akavache.Mobile
             registerFunction(() => akavacheDriver, typeof(ISuspensionDriver), null);
 
 #if APPKIT || UIKIT
+            BlobCache.ApplicationName = NSBundle.MainBundle.BundleIdentifier;
             registerFunction(() => new MacFilesystemProvider(), typeof(IFilesystemProvider), null);
 #endif
+
 #if ANDROID
+            var ai = Application.Context.PackageManager.GetApplicationInfo(Application.Context.PackageName, 0);
+            BlobCache.ApplicationName = ai.LoadLabel(Application.Context.PackageManager);
+
             registerFunction(() => new AndroidFilesystemProvider(), typeof(IFilesystemProvider), null);
 #endif
         }
