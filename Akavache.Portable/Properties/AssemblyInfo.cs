580c29d3d911a60f75debb277b9329a443e5027a
Paul Betts <paul@paulbetts.org>
2013-10-12 01:46:17 -0700

    Version bump to 3.1.1

--- Akavache.Portable/Properties/AssemblyInfo.cs
+++ Akavache.Portable/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.1.1")]
+[assembly: AssemblyFileVersion("3.1.1")]
 
 [assembly: InternalsVisibleTo("Akavache")]
 [assembly: InternalsVisibleTo("Akavache.Sqlite3")]
