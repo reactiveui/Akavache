b41da92271080b2f02a15ad618fc9c6d96b6bb49
Paul Betts <paul@paulbetts.org>
2013-10-11 22:53:59 -0700

    Copy pasta similar tests for object bulk ops

--- Akavache.Tests/BulkOperationsTest.cs
+++ Akavache.Tests/BulkOperationsTest.cs
@@ -106,7 +106,101 @@ namespace Akavache.Tests
         }
     }
 
-    class BlockingDisposeBulkCache : BlockingDisposeCache, IBulkBlobCache
+    public abstract class ObjectBulkOperationsTests
+    {
+        protected abstract IBlobCache CreateBlobCache(string path);
+
+        [Fact]
+        public void GetAsyncShouldWorkWithMultipleKeys()
+        {
+            var path = default(string);
+            using (Utility.WithEmptyDirectory(out path))
+            using (var fixture = CreateBlobCache(path))
+            {
+                var data = Tuple.Create("Foo", 4);
+                var keys = new[] { "Foo", "Bar", "Baz", };
+
+                foreach (var v in keys)
+                {
+                    fixture.InsertObject(v, data).First();
+                }
+
+                Assert.Equal(keys.Count(), fixture.GetAllKeys().Count());
+
+                var allData = fixture.GetObjectsAsync<Tuple<string, int>>(keys).First();
+
+                Assert.Equal(keys.Count(), allData.Count());
+                Assert.True(allData.All(x => x.Value.Item1 == data.Item1 && x.Value.Item2 == data.Item2));
+            }
+        }
+
+        [Fact]
+        public void GetAsyncShouldInvalidateOldKeys()
+        {
+            var path = default(string);
+            using (Utility.WithEmptyDirectory(out path))
+            using (var fixture = CreateBlobCache(path))
+            {
+                var data = Tuple.Create("Foo", 4);
+                var keys = new[] { "Foo", "Bar", "Baz", };
+
+                foreach (var v in keys)
+                {
+                    fixture.InsertObject(v, data, DateTimeOffset.MinValue).First();
+                }
+
+                var allData = fixture.GetObjectsAsync<Tuple<string, int>>(keys).First();
+                Assert.Equal(0, allData.Count());
+                Assert.Equal(0, fixture.GetAllKeys().Count());
+            }
+        }
+
+        [Fact]
+        public void InsertShouldWorkWithMultipleKeys()
+        {
+            var path = default(string);
+            using (Utility.WithEmptyDirectory(out path))
+            using (var fixture = CreateBlobCache(path))
+            {
+                var data = Tuple.Create("Foo", 4);
+                var keys = new[] { "Foo", "Bar", "Baz", };
+
+                fixture.InsertObjects(keys.ToDictionary(k => k, v => data)).First();
+
+                Assert.Equal(keys.Count(), fixture.GetAllKeys().Count());
+
+                var allData = fixture.GetObjectsAsync<Tuple<string, int>>(keys).First();
+
+                Assert.Equal(keys.Count(), allData.Count());
+                Assert.True(allData.All(x => x.Value.Item1 == data.Item1 && x.Value.Item2 == data.Item2));
+            }
+        }
+
+        [Fact]
+        public void InvalidateShouldTrashMultipleKeys()
+        {
+            var path = default(string);
+            using (Utility.WithEmptyDirectory(out path))
+            using (var fixture = CreateBlobCache(path))
+            {
+                var data = Tuple.Create("Foo", 4);
+                var keys = new[] { "Foo", "Bar", "Baz", };
+
+                foreach (var v in keys)
+                {
+                    fixture.InsertObject(v, data).First();
+                }
+
+                Assert.Equal(keys.Count(), fixture.GetAllKeys().Count());
+
+                fixture.InvalidateObjects<Tuple<string, int>>(keys).First();
+
+                Assert.Equal(0, fixture.GetAllKeys().Count());
+            }
+        }
+    }
+
+    class BlockingDisposeBulkCache : BlockingDisposeCache, IObjectBulkBlobCache
     {
         public BlockingDisposeBulkCache(IBlobCache inner) : base(inner) { }
 
@@ -128,6 +222,46 @@ namespace Akavache.Tests
         public IObservable<Unit> Invalidate(IEnumerable<string> keys)
         {
             return _inner.Invalidate(keys);
+        }
+
+        public IObservable<Unit> InsertObjects<T>(IDictionary<string, T> keyValuePairs, DateTimeOffset? absoluteExpiration = null)
+        {
+            return _inner.InsertObjects(keyValuePairs, absoluteExpiration);
+        }
+
+        public IObservable<IDictionary<string, T>> GetObjectsAsync<T>(IEnumerable<string> keys, bool noTypePrefix = false)
+        {
+            return _inner.GetObjectsAsync<T>(keys, noTypePrefix);
+        }
+
+        public IObservable<Unit> InvalidateObjects<T>(IEnumerable<string> keys)
+        {
+            return _inner.InvalidateObjects<T>(keys);
+        }
+
+        public IObservable<Unit> InsertObject<T>(string key, T value, DateTimeOffset? absoluteExpiration = null)
+        {
+            return _inner.InsertObject<T>(key, value, absoluteExpiration);
+        }
+
+        public IObservable<T> GetObjectAsync<T>(string key, bool noTypePrefix = false)
+        {
+            return _inner.GetObjectAsync<T>(key, noTypePrefix);
+        }
+
+        public IObservable<IEnumerable<T>> GetAllObjects<T>()
+        {
+            return _inner.GetAllObjects<T>();
+        }
+
+        public IObservable<Unit> InvalidateObject<T>(string key)
+        {
+            return _inner.InvalidateObject<T>(key);
+        }
+
+        public IObservable<Unit> InvalidateAllObjects<T>()
+        {
+            return _inner.InvalidateAllObjects<T>();
         }
     }
 
@@ -164,6 +298,46 @@ namespace Akavache.Tests
     }
 
     public class EncryptedSqliteBlobCacheBulkOperationsTests : BulkOperationsTests
+    {
+        protected override IBlobCache CreateBlobCache(string path)
+        {
+            return new BlockingDisposeBulkCache(new Akavache.Sqlite3.EncryptedBlobCache(Path.Combine(path, "sqlite.db")));
+        }
+    }
+
+    public class PersistentBlobCacheObjectBulkTests : ObjectBulkOperationsTests
+    {
+        protected override IBlobCache CreateBlobCache(string path)
+        {
+            return new BlockingDisposeBulkCache(new TPersistentBlobCache(path));
+        }
+    }
+
+    public class TestBlobCacheObjectBulkOperationsTests : ObjectBulkOperationsTests
+    {
+        protected override IBlobCache CreateBlobCache(string path)
+        {
+            return new BlockingDisposeBulkCache(new TestBlobCache(RxApp.TaskpoolScheduler));
+        }
+    }
+
+    public class EncryptedBlobCacheObjectBulkOperationsTests : ObjectBulkOperationsTests
+    {
+        protected override IBlobCache CreateBlobCache(string path)
+        {
+            return new BlockingDisposeBulkCache(new TEncryptedBlobCache(path));
+        }
+    }
+
+    public class SqliteBlobCacheObjectBulkOperationsTests : ObjectBulkOperationsTests
+    {
+        protected override IBlobCache CreateBlobCache(string path)
+        {
+            return new BlockingDisposeBulkCache(new SqlitePersistentBlobCache(Path.Combine(path, "sqlite.db")));
+        }
+    }
+
+    public class EncryptedSqliteBlobCacheObjectBulkOperationsTests : ObjectBulkOperationsTests
     {
         protected override IBlobCache CreateBlobCache(string path)
         {
