80ab94b5702dd1c8f4090f17b5c2da1fdc02114b
Paul Betts <paul@paulbetts.org>
2013-11-12 16:28:26 -0800

    First scratch at tests that all IHttpSchedulers should pass

---
+++ Akavache.Tests/Http/HttpSchedulerSharedTests.cs
@@ -0,0 +1,65 @@
+﻿using Akavache.Http;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Akavache.Http.Tests
+{
+    public abstract class HttpSchedulerSharedTests
+    {
+        protected abstract IHttpScheduler CreateFixture();
+
+        [Fact]
+        public void HttpSchedulerShouldCompleteADummyRequest()
+        {
+            var fixture = CreateFixture();
+
+            fixture.Client = new HttpClient(new TestHttpMessageHandler(() =>
+            {
+                var ret = new HttpResponseMessage()
+                {
+                    Content = new StringContent("foo", Encoding.UTF8),
+                    StatusCode = HttpStatusCode.OK,
+                };
+
+                ret.Headers.ETag = new EntityTagHeaderValue("\"worifjw\"");
+                return Observable.Return(ret);
+            }));
+
+            fixture.Client.BaseAddress = new Uri("http://example");
+
+            var rq = new HttpRequestMessage(HttpMethod.Get, "/");
+            var result = fixture.Schedule(rq, 1)
+                .Timeout(TimeSpan.FromSeconds(2.0), RxApp.TaskpoolScheduler)
+                .First();
+
+            Console.WriteLine(Encoding.UTF8.GetString(result.Item2));
+            Assert.Equal(HttpStatusCode.OK, result.Item1.StatusCode);
+            Assert.Equal(3 /*foo*/, result.Item2.Length);
+        }
+    }
+
+    public class BaseHttpSchedulerSharedTests : HttpSchedulerSharedTests
+    {
+        protected override IHttpScheduler CreateFixture()
+        {
+            return new HttpScheduler();
+        }
+    }
+
+    public class CachingHttpSchedulerSharedTests : HttpSchedulerSharedTests
+    {
+        protected override IHttpScheduler CreateFixture()
+        {
+            return new CachingHttpScheduler(new TestBlobCache(), new HttpScheduler());
+        }
+    }
+}
---
+++ Akavache.Tests/Http/TestHttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using System.Reactive.Threading.Tasks;
+    
+namespace Akavache.Http.Tests
+{
+    public class TestHttpMessageHandler : HttpMessageHandler
+    {
+        Func<IObservable<HttpResponseMessage>> block;
+        public TestHttpMessageHandler(Func<IObservable<HttpResponseMessage>> createResult)
+        {
+            block = createResult;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            // NB: We abuse await here so that the above throw gets marshalled 
+            // to a Task
+            return await block().ToTask();
+        }
+    }
+}
