24bed8521e69e65664fc54ce7376aeb3be12b69a
Paul Betts <paul@paulbetts.org>
2013-12-07 14:30:13 -0800

    Add Android version of data limits

--- Akavache.Http/Registrations.cs
+++ Akavache.Http/Registrations.cs
@@ -9,6 +9,12 @@ using ReactiveUI.Mobile;
 using ReactiveUI;
 using System.Reactive.Disposables;
 
+#if ANDROID
+using Android.App;
+using Android.Net;
+using Android.Telephony;
+#endif
+
 #if UIKIT
 using MonoTouch.SystemConfiguration;
 #endif
@@ -91,6 +97,49 @@ namespace Akavache.Http
             }
 
             return 10 * 1048576;
+        }
+#endif
+
+#if ANDROID
+        static long GetDataLimit()
+        {
+            var cm = Application.Context.GetSystemService(Application.ConnectivityService) as ConnectivityManager;
+            if (cm == null || cm.ActiveNetworkInfo == null || cm.ActiveNetworkInfo.IsRoaming) 
+            {
+                return 512 * 1024;
+            }
+
+            switch (cm.ActiveNetworkInfo.Type) 
+            {
+                case ConnectivityType.Mobile:
+                case ConnectivityType.MobileDun:
+                case ConnectivityType.MobileHipri:
+                    var tm = Application.Context.GetSystemService(Application.TelephonyService) as TelephonyManager;
+                    if (tm == null) 
+                    {
+                        return 512 * 1024;
+                    }
+                    switch (tm.NetworkType) 
+                    {
+                        case NetworkType.Hsdpa:
+                        case NetworkType.Hspap:
+                        case NetworkType.Hspa:
+                        case NetworkType.Cdma:
+                            return 2 * 1048576;
+                        case NetworkType.Lte:
+                            return 5 * 1048576;
+                        default:
+                            return 512 * 1024;
+                    }
+                case ConnectivityType.Bluetooth:
+                case ConnectivityType.Ethernet:
+                case ConnectivityType.Wifi:
+                case ConnectivityType.Wimax:
+                    return 5 * 1048576;
+
+                default:
+                    return 512 * 1024;
+            }
         }
 #endif
     }
