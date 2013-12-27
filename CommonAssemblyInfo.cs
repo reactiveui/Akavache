dfc9d8ba1a688097518ff4d53e69faecf6909215
Oren Novotny <oren@novotny.org>
2013-12-27 16:02:12 -0500

    Use correct pre-4.0 version of 3.99.0-beta

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.99.0")]
-[assembly: AssemblyFileVersion("2.99.0")]
+[assembly: AssemblyVersion("3.99.0")]
+[assembly: AssemblyFileVersion("3.99.0")]
