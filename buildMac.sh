432f12469b02db2c6fac4d6485e3a5c65f40c674
Oren Novotny <oren@novotny.org>
2013-12-31 09:11:42 -0500

    Fix build config param so it actually builds release mode

--- buildMac.sh
+++ buildMac.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool build '--configuration:Release|AnyCPU' Akavache_XSAll.sln
+/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool build '--configuration:Release' Akavache_XSAll.sln
