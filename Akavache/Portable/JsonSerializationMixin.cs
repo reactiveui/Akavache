reactiveui/Akavache

commit 83b9f58b2ee106b3625471bbe162397c810f9be9

Rewrite async SelectMany to use pure Rx
ReactiveUI patches in awaiters for Rx, we don't have them here until we
upgrade to Rx 2.2.2

Akavache/Portable/JsonSerializationMixin.cs
@@ -210,18 +210,20 @@ namespace Akavache
             var fetch = Observable.Defer(() => This.GetObjectCreatedAt<T>(key))
                 .Select(x => fetchPredicate == null || x == null || fetchPredicate(x.Value))
                 .Where(x => x != false)
-                .SelectMany(async _ => {
+                .SelectMany(_ => 
+                {
                     var ret = default(T);
-                    try {
-                        ret = await fetchFunc();
-                    } catch (Exception) {
-                        if (shouldInvalidateOnError) This.InvalidateObject<T>(key);
-                        throw;
-                    }
+                    var fetchObs = fetchFunc().Catch<T, Exception>(ex =>
+                    {
+                        var shouldInvalidate = shouldInvalidateOnError ?
+                            This.InvalidateObject<T>(key) :
+                            Observable.Return(Unit.Default);
+                        return shouldInvalidate.SelectMany(__ => Observable.Throw<T>(ex));
+                    });
 
-                    await This.InvalidateObject<T>(key);
-                    await This.InsertObject(key, ret, absoluteExpiration);
-                    return ret;
+                    return fetchObs
+                        .SelectMany(x => This.InvalidateObject<T>(key).Select(__ => x))
+                        .SelectMany(x => This.InsertObject(key, ret, absoluteExpiration).Select(__ => x));
                 });
 
             var result = This.GetObjectAsync<T>(key).Select(x => new Tuple<T, bool>(x, true))

commit 4b1e73e54d54c51a278aa90c798035a6b7cb7647

Fix bug where the cache would never be updated
The fetched result was never inserted...
Bug was introduced in 83b9f58b2ee106b.

Akavache/Portable/JsonSerializationMixin.cs
@@ -224,7 +224,7 @@ namespace Akavache
 
                     return fetchObs
                         .SelectMany(x => This.InvalidateObject<T>(key).Select(__ => x))
-                        .SelectMany(x => This.InsertObject(key, ret, absoluteExpiration).Select(__ => x));
+                        .SelectMany(x => This.InsertObject(key, x, absoluteExpiration).Select(__ => x));
                 });
 
             var result = This.GetObject<T>(key).Select(x => new Tuple<T, bool>(x, true))