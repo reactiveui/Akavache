fc704a6fec2da55988d5138363351142214704b5
Paul Betts <paul@paulbetts.org>
2014-06-24 19:26:21 -0700

    Implement encryption facade

---
+++ Akavache/Facades_Desktop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akavache
+{
+    public static class Encryption
+    {
+        public static Task<byte[]> EncryptBlock(byte[] block)
+        {
+            var tcs = new TaskCompletionSource<byte[]>();
+            try {
+                tcs.TrySetResult(ProtectedData.Protect(block, null, DataProtectionScope.CurrentUser));
+            } catch (Exception ex) {
+                tcs.TrySetException(ex);
+            }
+
+            return tcs.Task;
+        }
+
+        public static Task<byte[]> DecryptBlock(byte[] block)
+        {
+            var tcs = new TaskCompletionSource<byte[]>();
+            try {
+                tcs.TrySetResult(ProtectedData.Unprotect(block, null, DataProtectionScope.CurrentUser));
+            } catch (Exception ex) {
+                tcs.TrySetException(ex);
+            }
+
+            return tcs.Task;
+        }
+    }
+}
