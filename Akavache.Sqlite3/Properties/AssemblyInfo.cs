8c3d1ae26b9f324a120f4d8f5740ff4d5823bf2b
Paul Betts <paul@paulbetts.org>
2014-06-24 18:42:40 -0700

    SQLite3 is now a PCL itself

--- Akavache.Sqlite3/Properties/AssemblyInfo.cs
+++ Akavache.Sqlite3/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fcf01db6-c782-4773-b9fd-2f9ed197c254")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
