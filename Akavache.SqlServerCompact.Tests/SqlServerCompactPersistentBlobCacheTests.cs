2e91172e5fb74d42c26587676f0bd153cc6548ad
Brendan Forster <brendan@github.com>
2014-07-02 05:43:21 +0930

    wireup a simple test

---
+++ Akavache.SqlServerCompact.Tests/SqlServerCompactPersistentBlobCacheTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Akavache.Tests;
+using Xunit;
+
+namespace Akavache.SqlServerCompact.Tests
+{
+    public class SqlServerCompactPersistentBlobCacheTests
+    {
+        [Fact]
+        public void FailTheThing()
+        {
+            string directory;
+            using (Utility.WithEmptyDirectory(out directory))
+            {
+                var file = Path.Combine(directory, "database.sdf");
+                using (var fixture = new SqlServerCompactPersistentBlobCache(file))
+                {
+                    Assert.True(false);
+                }
+            }
+        }
+    }
+}
