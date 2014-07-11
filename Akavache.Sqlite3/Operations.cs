b9739da91e9737c5fabb5ecec657d7ef36ea5f12
Paul Betts <paul@paulbetts.org>
2014-07-11 14:07:14 -0700

    Add more operations, make sure Reset is in a finally block

--- Akavache.Sqlite3/Operations.cs
+++ Akavache.Sqlite3/Operations.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Subjects;
 using System.Text;
@@ -31,7 +32,7 @@ namespace Akavache.Sqlite3
 
             if (result != SQLite3.Result.OK) 
             {
-                throw new SQLiteException(result, "Couldn't prepare insert statement");
+                throw new SQLiteException(result, "Couldn't prepare statement");
             }
 
             inner = insertOp;
@@ -41,30 +42,36 @@ namespace Akavache.Sqlite3
         {
             var insertList = toInsert.ToList();
 
-            return new Action(() => 
+            return () => 
             {
                 foreach (var v in insertList) 
                 {
-                    this.Checked(raw.sqlite3_bind_text(insertOp, 0, v.Key));
-
-                    if (String.IsNullOrWhiteSpace(v.TypeName)) 
+                    try 
                     {
-                        this.Checked(raw.sqlite3_bind_null(insertOp, 1));
+                        this.Checked(raw.sqlite3_bind_text(insertOp, 0, v.Key));
+
+                        if (String.IsNullOrWhiteSpace(v.TypeName)) 
+                        {
+                            this.Checked(raw.sqlite3_bind_null(insertOp, 1));
+                        } 
+                        else 
+                        {
+                            this.Checked(raw.sqlite3_bind_text(insertOp, 1, v.TypeName));
+                        }
+
+                        this.Checked(raw.sqlite3_bind_text(insertOp, 1, v.TypeName ?? ""));
+                        this.Checked(raw.sqlite3_bind_blob(insertOp, 2, v.Value));
+                        this.Checked(raw.sqlite3_bind_int64(insertOp, 3, v.Expiration.ToUniversalTime().Ticks));
+                        this.Checked(raw.sqlite3_bind_int64(insertOp, 4, v.CreatedAt.ToUniversalTime().Ticks));
+
+                        this.Checked(raw.sqlite3_step(insertOp));
                     } 
-                    else 
+                    finally 
                     {
-                        this.Checked(raw.sqlite3_bind_text(insertOp, 1, v.TypeName));
+                        this.Checked(raw.sqlite3_reset(insertOp));
                     }
-
-                    this.Checked(raw.sqlite3_bind_text(insertOp, 1, v.TypeName ?? ""));
-                    this.Checked(raw.sqlite3_bind_blob(insertOp, 2, v.Value));
-                    this.Checked(raw.sqlite3_bind_int64(insertOp, 3, v.Expiration.ToUniversalTime().Ticks));
-                    this.Checked(raw.sqlite3_bind_int64(insertOp, 4, v.CreatedAt.ToUniversalTime().Ticks));
-
-                    this.Checked(raw.sqlite3_step(insertOp));
-                    this.Checked(raw.sqlite3_reset(insertOp));
                 }
-            });
+            };
         }
 
         public void Dispose()
@@ -78,17 +85,18 @@ namespace Akavache.Sqlite3
         sqlite3_stmt[] selectOps = null;
         IDisposable inner;
 
-        public BulkSelectSqliteOperation(SQLiteConnection conn)
+        public BulkSelectSqliteOperation(SQLiteConnection conn, bool useTypeInsteadOfKey)
         {
             var qs = new StringBuilder("?");
+            var column = useTypeInsteadOfKey ? "TypeName" : "Key";
 
             selectOps = Enumerable.Range(1, Constants.OperationQueueChunkSize)
                 .Select(x => {
                     var stmt = default(sqlite3_stmt);
                     var result = (SQLite3.Result)raw.sqlite3_prepare_v2(conn.Handle,
-                        String.Format("SELECT TypeName,Value,Expiration,CreatedAt FROM CacheElement WHERE Key In ({0})", qs), out stmt);
+                        String.Format("SELECT Value,Expiration FROM CacheElement WHERE {0} In ({1})", column, qs), out stmt);
 
-                    if (result != SQLite3.Result.OK) throw new SQLiteException(result, "Couldn't prepare insert statement");
+                    if (result != SQLite3.Result.OK) throw new SQLiteException(result, "Couldn't prepare statement");
 
                     qs.Append(",?");
                     return stmt;
@@ -107,26 +115,31 @@ namespace Akavache.Sqlite3
             return (() => 
             {
                 var result = new List<CacheElement>();
-                for (int i = 0; i < selectList.Count; i++) 
+                try 
                 {
-                    this.Checked(raw.sqlite3_bind_text(selectOp, i, selectList[i]));
+                    for (int i = 0; i < selectList.Count; i++) 
+                    {
+                        this.Checked(raw.sqlite3_bind_text(selectOp, i, selectList[i]));
+                    }
+
+                    int idx = 0;
+                    while (this.Checked(raw.sqlite3_step(selectOp)) == SQLite3.Result.Row) 
+                    {
+                        var key = selectList[idx++];
+                        var ce = new CacheElement() {
+                            Key = key, TypeName = key,
+                            Value = raw.sqlite3_column_blob(selectOp, 1),
+                            Expiration = new DateTime(raw.sqlite3_column_int64(selectOp, 2)),
+                        };
+
+                        result.Add(ce);
+                    }
+                } 
+                finally 
+                {
+                    this.Checked(raw.sqlite3_reset(selectOp));
                 }
 
-                int idx = 0;
-                while (this.Checked(raw.sqlite3_step(selectOp)) == SQLite3.Result.Row) 
-                {
-                    var ce = new CacheElement() {
-                        Key = selectList[idx++],
-                        TypeName = raw.sqlite3_column_text(selectOp, 0),
-                        Value = raw.sqlite3_column_blob(selectOp, 1),
-                        Expiration = new DateTime(raw.sqlite3_column_int64(selectOp, 2)),
-                        CreatedAt = new DateTime(raw.sqlite3_column_int64(selectOp, 3)),
-                    };
-
-                    result.Add(ce);
-                }
-
-                this.Checked(raw.sqlite3_reset(selectOp));
                 return result;
             });
         }
@@ -142,17 +155,18 @@ namespace Akavache.Sqlite3
         sqlite3_stmt[] deleteOps = null;
         IDisposable inner;
 
-        public BulkInvalidateSqliteOperation(SQLiteConnection conn)
+        public BulkInvalidateSqliteOperation(SQLiteConnection conn, bool useTypeInsteadOfKey)
         {
             var qs = new StringBuilder("?");
 
+            var column = useTypeInsteadOfKey ? "TypeName" : "Key";
             deleteOps = Enumerable.Range(1, Constants.OperationQueueChunkSize)
                 .Select(x => {
                     var stmt = default(sqlite3_stmt);
                     var result = (SQLite3.Result)raw.sqlite3_prepare_v2(conn.Handle,
-                        String.Format("DELETE FROM CacheElement WHERE Key In ({0})", qs), out stmt);
+                        String.Format("DELETE FROM CacheElement WHERE {0} In ({1})", column, qs), out stmt);
 
-                    if (result != SQLite3.Result.OK) throw new SQLiteException(result, "Couldn't prepare insert statement");
+                    if (result != SQLite3.Result.OK) throw new SQLiteException(result, "Couldn't prepare statement");
 
                     qs.Append(",?");
                     return stmt;
@@ -170,14 +184,129 @@ namespace Akavache.Sqlite3
             var deleteOp = deleteOps[deleteList.Count - 1];
             return new Action(() => 
             {
-                for (int i = 0; i < deleteList.Count; i++) 
+                try 
                 {
-                    this.Checked(raw.sqlite3_bind_text(deleteOp, i, deleteList[i]));
+                    for (int i = 0; i < deleteList.Count; i++) 
+                    {
+                        this.Checked(raw.sqlite3_bind_text(deleteOp, i, deleteList[i]));
+                    }
+
+                    this.Checked(raw.sqlite3_step(deleteOp));
+                } 
+                finally 
+                {
+                    this.Checked(raw.sqlite3_reset(deleteOp));
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref inner, Disposable.Empty).Dispose();
+        }
+    }
+
+    sealed class InvalidateAllSqliteOperation : IPreparedSqliteOperation
+    {
+        SQLiteConnection conn;
+        IDisposable inner;
+
+        public InvalidateAllSqliteOperation(SQLiteConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        public Action PrepareToExecute(IEnumerable<CacheElement> toInsert)
+        {
+            return () => this.Checked(raw.sqlite3_exec(conn.Handle, "DELETE FROM CacheElement"));
+        }
+
+        public void Dispose() { }
+    }
+
+    sealed class VacuumSqliteOperation : IPreparedSqliteOperation
+    {
+        sqlite3_stmt vacuumOp = null;
+        IScheduler scheduler;
+        IDisposable inner;
+
+        public VacuumSqliteOperation(SQLiteConnection conn, IScheduler scheduler)
+        {
+            var result = (SQLite3.Result)raw.sqlite3_prepare_v2(conn.Handle, "DELETE FROM CacheElement WHERE Expiration < ?; VACUUM", out vacuumOp);
+
+            if (result != SQLite3.Result.OK) 
+            {
+                throw new SQLiteException(result, "Couldn't prepare statement");
+            }
+
+            this.scheduler = scheduler;
+            inner = vacuumOp;
+        }
+
+        public Action PrepareToExecute()
+        {
+            var now = scheduler.Now.UtcTicks;
+
+            return new Action(() => 
+            {
+                try 
+                {
+                    this.Checked(raw.sqlite3_bind_int64(vacuumOp, 0, now));
+                    this.Checked(raw.sqlite3_step(vacuumOp));
+                } 
+                finally 
+                {
+                    this.Checked(raw.sqlite3_reset(vacuumOp));
+                }
+            });
+        }
+
+        public void Dispose()
+        {
+            Interlocked.Exchange(ref inner, Disposable.Empty).Dispose();
+        }
+    }
+
+    sealed class GetKeysSqliteOperation : IPreparedSqliteOperation
+    {
+        sqlite3_stmt selectOp = null;
+        IScheduler scheduler;
+        IDisposable inner;
+
+        public GetKeysSqliteOperation(SQLiteConnection conn, IScheduler scheduler)
+        {
+            var result = (SQLite3.Result)raw.sqlite3_prepare_v2(conn.Handle, "SELECT Key FROM CacheElement WHERE Expiration >= ?", out selectOp);
+
+            if (result != SQLite3.Result.OK) 
+            {
+                throw new SQLiteException(result, "Couldn't prepare statement");
+            }
+
+            inner = selectOp;
+            this.scheduler = scheduler;
+        }
+
+        public Func<List<string>> PrepareToExecute()
+        {
+            return () => 
+            {
+                var result = new List<string>();
+                try 
+                {
+                    this.Checked(raw.sqlite3_bind_int64(selectOp, 0, scheduler.Now.UtcTicks));
+
+                    while (this.Checked(raw.sqlite3_step(selectOp)) == SQLite3.Result.Row) 
+                    {
+                        result.Add(raw.sqlite3_column_text(selectOp, 0));
+                    }
+                } 
+                finally 
+                {
+                    this.Checked(raw.sqlite3_reset(selectOp));
                 }
 
-                this.Checked(raw.sqlite3_step(deleteOp));
-                this.Checked(raw.sqlite3_reset(deleteOp));
-            });
+                return result;
+            };
         }
 
         public void Dispose()
