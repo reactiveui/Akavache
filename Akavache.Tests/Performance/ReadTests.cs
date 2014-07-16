0c1fa4b39b387e161244082882936579d406b2f5
Paul Betts <paul@paulbetts.org>
2014-07-16 16:24:28 -0700

    Move read tests to Next

--- Akavache.Tests/Performance/ReadTests.cs
+++ Akavache.Tests/Performance/ReadTests.cs
@@ -131,7 +131,7 @@ namespace Akavache.Tests.Performance
     {
         protected override IBlobCache CreateBlobCache(string path)
         {
-            return new SqlitePersistentBlobCache(Path.Combine(path, "blob.db"));
+            return new SqlitePersistentBlobCacheNext(Path.Combine(path, "blob.db"));
         }
     }
 }
