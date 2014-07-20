fc9936487181acaa25df6b2dba6b3805faa6edda
Paul Betts <paul@paulbetts.org>
2014-07-20 04:15:26 -0700

    Implement bulk'ifying of unrelated requests with the same operation

--- Akavache.Sqlite3/OperationQueueCoalescing.cs
+++ Akavache.Sqlite3/OperationQueueCoalescing.cs
@@ -26,6 +26,8 @@ namespace Akavache.Sqlite3
 
         static internal List<Tuple<OperationType, IEnumerable, object>> CoalesceOperations(List<Tuple<OperationType, IEnumerable, object>> inputItems)
         {
+            var ret = new List<Tuple<OperationType, IEnumerable, object>>();
+
             // 1. GroupBy key, then by original order
             var groupedOps = new Dictionary<string, List<Tuple<OperationType, IEnumerable, object>>>();
             foreach (var v in inputItems)
@@ -54,9 +56,52 @@ namespace Akavache.Sqlite3
                 if (key == nullKey) continue;
 
                 groupedOps[key] = toDedup
-                    .Aggregate(groupedOps[key], (acc, x) => MultipleOpsTurnIntoSingleOp(acc, x))
+                    .Aggregate((IEnumerable<Tuple<OperationType, IEnumerable, object>>)groupedOps[key], (acc, x) => MultipleOpsTurnIntoSingleOp(acc, x))
                     .ToList();
             }
+
+            while (groupedOps.Count > 0)
+            {
+                var toProcess = new List<Tuple<OperationType, IEnumerable, object>>();
+                var toRemove = new List<string>();
+
+                // 3. Take all the *first* items from every group 
+                foreach (var key in groupedOps.Keys)
+                {
+                    var list = groupedOps[key];
+
+                    toProcess.Add(list[0]);
+                    list.RemoveAt(0);
+                    if (list.Count == 0) toRemove.Add(key);
+                }
+
+                // 4. Group by request type (insert, etc)
+                var finalItems = CoalesceUnrelatedItems(toProcess);
+
+                // 5. Yield the ops out
+                ret.AddRange(finalItems);
+
+                foreach (var v in toRemove) groupedOps.Remove(v);
+            }
+
+            return ret;
+        }
+
+        static IEnumerable<Tuple<OperationType, IEnumerable, object>> CoalesceUnrelatedItems(IEnumerable<Tuple<OperationType, IEnumerable, object>> items)
+        {
+            return items.GroupBy(x => x.Item1)
+                .SelectMany(group =>
+                {
+                    switch (group.Key)
+                    {
+                        case OperationType.BulkInsertSqliteOperation:
+                            return new[] { GroupUnrelatedInserts(group) };
+                        case OperationType.BulkInvalidateSqliteOperation:
+                            return new[] { GroupUnrelatedDeletes(group) };
+                        default:
+                            return (IEnumerable<Tuple<OperationType, IEnumerable, object>>)group;
+                    }
+                });
         }
 
         static IEnumerable<Tuple<OperationType, IEnumerable, object>> MultipleOpsTurnIntoSingleOp(IEnumerable<Tuple<OperationType, IEnumerable, object>> itemsWithSameKey, OperationType opTypeToDedup)
@@ -86,6 +131,39 @@ namespace Akavache.Sqlite3
                     op
                 };
             });
+        }
+
+        static Tuple<OperationType, IEnumerable, object> GroupUnrelatedSelects(IEnumerable<Tuple<OperationType, IEnumerable, object>> unrelatedInserts)
+        {
+            throw new NotImplementedException();
+        }
+
+        static Tuple<OperationType, IEnumerable, object> GroupUnrelatedInserts(IEnumerable<Tuple<OperationType, IEnumerable, object>> unrelatedInserts)
+        {
+            var subj = new AsyncSubject<Unit>();
+
+            var elements = unrelatedInserts.SelectMany(x =>
+            {
+                subj.Subscribe((AsyncSubject<Unit>)x.Item3);
+                return (IEnumerable<CacheElement>)x.Item2;
+            }).ToList();
+ 
+            return new Tuple<OperationType, IEnumerable, object>(
+                OperationType.BulkInsertSqliteOperation, elements, subj);
+        }
+
+        static Tuple<OperationType, IEnumerable, object> GroupUnrelatedDeletes(IEnumerable<Tuple<OperationType, IEnumerable, object>> unrelatedDeletes)
+        {
+            var subj = new AsyncSubject<Unit>();
+
+            var elements = unrelatedDeletes.SelectMany(x =>
+            {
+                subj.Subscribe((AsyncSubject<Unit>)x.Item3);
+                return (IEnumerable<string>)x.Item2;
+            }).ToList();
+ 
+            return new Tuple<OperationType, IEnumerable, object>(
+                OperationType.BulkInsertSqliteOperation, elements, subj);
         }
 
         static string GetKeyFromTuple(Tuple<OperationType, IEnumerable, object> item)
