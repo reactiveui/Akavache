c86a02662452aab69407c13efaecb50284131a70
Paul Betts <paul@paulbetts.org>
2014-08-08 17:07:27 +0200

    Version bump to 4.0.1

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyFileVersion("4.0.1")]
