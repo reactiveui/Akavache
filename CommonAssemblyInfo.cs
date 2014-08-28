0fbb81e189fd762ea53f69f0bf3393327e379bd7
Paul Betts <paul@paulbetts.org>
2014-08-28 16:05:22 -0700

    I am an idiot.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.0.3.1")]
-[assembly: AssemblyFileVersion("4.0.3.1")]
+[assembly: AssemblyVersion("4.0.3.2")]
+[assembly: AssemblyFileVersion("4.0.3.2")]
