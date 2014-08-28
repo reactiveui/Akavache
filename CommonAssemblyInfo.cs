0e32222adf5f6babccf164fe276e2fe5b75beb89
Paul Betts <paul@paulbetts.org>
2014-08-28 15:56:15 -0700

    Hotfix 4.0.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("4.0.3")]
-[assembly: AssemblyFileVersion("4.0.3")]
+[assembly: AssemblyVersion("4.0.3.1")]
+[assembly: AssemblyFileVersion("4.0.3.1")]
