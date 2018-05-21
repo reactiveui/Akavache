45c580cf12b564afa53485168daa2306aef1b72c
Shane Neuville <shane94@hotmail.com>
2018-05-20 23:31:48 -0600

    Update nuget packages and build scripts (#409)
    * updating frameworks and build scripts
    
    * git ignore and json versioning

---
+++ src/Akavache.Core/Platforms/netstandard2.0/Facades.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akavache
+{
+    public static class Utility
+    {
+        public static string GetMd5Hash(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IObservable<T> LogErrors<T>(this IObservable<T> This, string message = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IObservable<Unit> CopyToAsync(this Stream This, Stream destination, IScheduler scheduler = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
