554942ec4a0137658381d4bcd07f7468e299811b
Unknown <erik.hooper@schneider-electric.com>
2018-10-10 15:28:03 -0600

    Fix for encryption issue

--- src/Akavache.Core/Platforms/shared/EncryptionProvider.cs
+++ src/Akavache.Core/Platforms/shared/EncryptionProvider.cs
@@ -9,6 +9,20 @@ using System.Threading.Tasks;
 
 namespace Akavache
 {
+#if PORTABLE ||NET_461
+        public class EncryptionProvider : IEncryptionProvider
+    {
+        public IObservable<byte[]> EncryptBlock(byte[] block)
+        {
+            return Observable.Return(System.Security.Cryptography.ProtectedData.Protect(block, null, System.Security.Cryptography.DataProtectionScope.CurrentUser));
+        }
+
+        public IObservable<byte[]> DecryptBlock(byte[] block)
+        {
+            return Observable.Return(System.Security.Cryptography.ProtectedData.Unprotect(block, null, System.Security.Cryptography.DataProtectionScope.CurrentUser));
+        }
+    }
+#else
     public class EncryptionProvider : IEncryptionProvider
     {
         public IObservable<byte[]> EncryptBlock(byte[] block)
@@ -21,4 +35,6 @@ namespace Akavache
             return Observable.Return(ProtectedData.Unprotect(block, null, DataProtectionScope.CurrentUser));
         }
     }
+#endif
+
 }
