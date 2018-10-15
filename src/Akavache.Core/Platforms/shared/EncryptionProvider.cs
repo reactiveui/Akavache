08253698bcbc7501ed38c483cdbbf863f5120ed8
Unknown <erik.hooper@schneider-electric.com>
2018-10-15 11:24:30 -0600

    Fix name, and simplify encryption provider class
    Changes from pull request.

--- src/Akavache.Core/Platforms/shared/EncryptionProvider.cs
+++ src/Akavache.Core/Platforms/shared/EncryptionProvider.cs
@@ -1,40 +1,26 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+#if NET_461
+using RxCrypt = System.Security.Cryptography;
+#else
+using RxCrypt = Akavache;
+#endif
 
 namespace Akavache
 {
-#if PORTABLE ||NET_461
         public class EncryptionProvider : IEncryptionProvider
     {
         public IObservable<byte[]> EncryptBlock(byte[] block)
         {
-            return Observable.Return(System.Security.Cryptography.ProtectedData.Protect(block, null, System.Security.Cryptography.DataProtectionScope.CurrentUser));
+            return Observable.Return(RxCrypt.ProtectedData.Protect(block, null, RxCrypt.DataProtectionScope.CurrentUser));
         }
 
         public IObservable<byte[]> DecryptBlock(byte[] block)
         {
-            return Observable.Return(System.Security.Cryptography.ProtectedData.Unprotect(block, null, System.Security.Cryptography.DataProtectionScope.CurrentUser));
+            return Observable.Return(RxCrypt.ProtectedData.Unprotect(block, null, RxCrypt.DataProtectionScope.CurrentUser));
         }
     }
-#else
-    public class EncryptionProvider : IEncryptionProvider
-    {
-        public IObservable<byte[]> EncryptBlock(byte[] block)
-        {
-            return Observable.Return(ProtectedData.Protect(block, null, DataProtectionScope.CurrentUser));
-        }
 
-        public IObservable<byte[]> DecryptBlock(byte[] block)
-        {
-            return Observable.Return(ProtectedData.Unprotect(block, null, DataProtectionScope.CurrentUser));
-        }
-    }
-#endif
 
 }
