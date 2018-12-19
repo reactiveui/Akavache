c93d63f614dc9c54836fc672939631c89edb5860
ShaneN <shane94@hotmail.com>
2018-12-18 18:24:55 -0700

    fix line endings

--- src/Akavache.Core/PreserveAttribute.cs
+++ src/Akavache.Core/PreserveAttribute.cs
@@ -4,5 +4,20 @@ using System.Text;
 
 namespace Akavache.Core
 {
-	[AttributeUsage(AttributeTargets.All)]	internal class PreserveAttribute : Attribute	{		public bool AllMembers;		public bool Conditional;		public PreserveAttribute(bool allMembers, bool conditional)		{			AllMembers = allMembers;			Conditional = conditional;		}		public PreserveAttribute()		{		}	}
+    [AttributeUsage(AttributeTargets.All)]
+    internal class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+
+        public PreserveAttribute(bool allMembers, bool conditional)
+        {
+            AllMembers = allMembers;
+            Conditional = conditional;
+        }
+
+        public PreserveAttribute()
+        {
+        }
+    }
 }
