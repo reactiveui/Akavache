8316300904dbfe6eee0c5c468f115029aeafa1b9
Chris Pulman <chris.pulman@yahoo.com>
2025-08-27 01:22:07 +0100

    Remove SQLitePCL.Batteries

--- src/Akavache.Core/Core/AkavacheBuilder.cs
+++ src/Akavache.Core/Core/AkavacheBuilder.cs
@@ -21,7 +21,6 @@ internal class AkavacheBuilder : IAkavacheBuilder
     [SuppressMessage("ExecutingAssembly.Location", "IL3000:String may be null", Justification = "Handled.")]
     public AkavacheBuilder()
     {
-        SQLitePCL.Batteries_V2.Init();
         string? fileLocation;
         try
         {
