2ed0f39d5551966eeeb835d8c8509fbd7c0393b7
dyren <572917108@qq.com>
2026-01-18 15:29:59 +0800

    Remove SQLite dependency from Akavache

--- src/Akavache.Sqlite3/CacheEntry.cs
+++ src/Akavache.Sqlite3/CacheEntry.cs
@@ -1,7 +1,9 @@
-// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
+﻿// Copyright (c) 2025 .NET Foundation and Contributors. All rights reserved.
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
+
+using SQLite;
 
 namespace Akavache;
 
@@ -13,6 +15,8 @@ internal class CacheEntry
     /// <summary>
     /// Gets or sets the unique identifier for the cache entry.
     /// </summary>
+    [PrimaryKey]
+    [Unique]
     public string? Id { get; set; }
 
     /// <summary>
@@ -23,11 +27,13 @@ internal class CacheEntry
     /// <summary>
     /// Gets or sets the date and time when the cache entry will expire.
     /// </summary>
+    [Indexed]
     public DateTimeOffset? ExpiresAt { get; set; }
 
     /// <summary>
     /// Gets or sets the type name associated with the cache entry.
     /// </summary>
+    [Indexed]
     public string? TypeName { get; set; }
 
     /// <summary>
